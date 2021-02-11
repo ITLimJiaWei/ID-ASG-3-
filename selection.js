22deb0d4172939c3966b2313be6bf84823f3c44f
Chua DongEn <s10202623@connect.np.edu.sg>
2021-02-11 23:02:28 +0800

    PokeApi js

--- selection.js
+++ selection.js
@@ -1,14 +1,29 @@
 //Testing PokeAPI 
 
-var settings = {
-    "url": "https://pokeapi.co/api/v2/pokemon/pikachu",
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-      "Cookie": "__cfduid=d15aa94cfa2c415a3913143f52535964c1612860365"
-    },
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
+$(document).ready(function () {
+
+  var starters = ["pikachu","charmander","bulbasaur","squirtle"]
+  var i;
+  for (i = 0; i < starters.length; i++) {
+    let pokemonName = starters[i];
+    let url = "https://pokeapi.co/api/v2/pokemon/"+pokemonName;
+    var settings = {
+      "url": url,
+      "method": "GET",
+      "timeout": 0,
+      
+    };
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response)
+      let id = response.id;
+      console.log(id);
+      let sprite = response.sprites.front_default;
+      //sprite = response.sprites.other["official-artwork"].front_default; //Official artwork ver
+      console.log(sprite);
+      let elementid = "#" + pokemonName;
+      $(elementid).attr("src",sprite);
+    });
+  }
+
   });
